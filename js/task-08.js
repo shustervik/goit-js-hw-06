9da9811937f9d82e701b03b235a1e197120b737c
Shustrova Viktoriia <shustrova.vik9@gmail.com>
2022-04-03 18:49:22 +0300

    Редактирует task 8

--- js/task-08.js
+++ js/task-08.js
@@ -9,7 +9,7 @@ function onFormSubmit(event) {
     elements: { email, password }
   } = event.currentTarget;
   if (email.value === "" || password.value === "") {
-    return console.log("Alert! Please fill in all the fields!");
+    return alert("Please fill in all the fields!");
   }
   console.log(`Email: ${email.value}, Password: ${password.value}`);
   event.currentTarget.reset();
